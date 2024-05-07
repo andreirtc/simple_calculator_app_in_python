973d9007e247c979923ca3b13a3e169123359a10
andreirtc <ralphandreicastillo326@gmail.com>
2024-05-08 00:38:48 +0800

    Pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,6 @@
+# 1. Import tkinter for the simple chatbox GUI
+# 2. Create a class for the simple calculator
+# 3. Build the chatbox GUI (height and width, chat frame, enter button, and bind the enter or return key)
+# 4. Define a function to initialize chat
+# 5. Define functions to add and send message. Under function send message, use if-else and exception handling in case of wrong input or zero division error.
+# 6. Define a function to calculate and output results
